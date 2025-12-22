88a07aa36f54a19e80c5688a0f70288ae1aa6b3f
jh-k02 <76684103+jh-k02@users.noreply.github.com>
2025-12-22 10:09:44 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,15 +7,42 @@ let package = Package(
         .iOS(.v12)
     ],
     products: [
+        // 외부에 노출되는 제품은 래퍼 타깃을 노출
         .library(
             name: "CaulySDK",
-            targets: ["CaulySDK"]
+            targets: ["CaulySDKWrapper"]
         )
     ],
     targets: [
+        // 1) 실제 바이너리
         .binaryTarget(
             name: "CaulySDK",
             path: "CaulySDK.xcframework"
+        ),
+
+        // 2) 래퍼 타깃: 링크 설정을 강제
+        .target(
+            name: "CaulySDKWrapper",
+            dependencies: [
+                .target(name: "CaulySDK")
+            ],
+            linkerSettings: [
+                // Required
+                .linkedFramework("AVKit"),
+                .linkedFramework("UIKit"),
+                .linkedFramework("Foundation"),
+                .linkedFramework("CoreGraphics"),
+                .linkedFramework("QuartzCore"),
+                .linkedFramework("SystemConfiguration"),
+                .linkedFramework("MediaPlayer"),
+                .linkedFramework("CFNetwork"),
+                .linkedFramework("MessageUI"),
+
+                // Optional(Weak)
+                .linkedFramework("EventKit", .weak),
+                .linkedFramework("AdSupport", .weak),
+                .linkedFramework("MessageUI", .weak),
+            ]
         )
     ]
 )
